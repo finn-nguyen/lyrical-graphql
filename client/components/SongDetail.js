cc2664c8614bfa3355e534fdaac1370d87b0a42e
nvtuongcs <nvtuongcs@gmail.com>
2019-01-18 17:24:31 +0700

    Fetch lyric

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import { Link } from 'react-router'
 import fetchSong from '../queries/fetchSong'
+import LyricCreate from './LyricCreate'
+import LyricList from './LyricList'
 
 class SongDetail extends Component {
   render() {
     const { song } = this.props.data
+    console.log({song})
     if (!song) {
       return <div>Loading...</div>
     }
@@ -13,6 +16,8 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
+        <LyricCreate songId={song.id}/>
       </div>
     )
   }
